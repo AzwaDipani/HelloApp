60af9ca43fdb5c5aeb79ad02c5377c553db0e7f7
AzwaDipani <destadwiputra3@gmail.com>
2023-12-23 22:21:19 +0700

    penambahan

--- app/src/main/java/com/hello/fragmentmain.java
+++ app/src/main/java/com/hello/fragmentmain.java
@@ -1,25 +1,41 @@
 package com.hello;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.widget.ViewPager2;
-
 import android.os.Bundle;
-
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.appcompat.widget.Toolbar;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 public class fragmentmain extends AppCompatActivity {
 
     TabLayout tabLayout;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
     ViewPager2 viewPager2;
     ViewPagerAdapter viewPagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragmentmain);
-        tabLayout =findViewById(R.id.tabLayout);
-        viewPager2 =findViewById(R.id.viewPager);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager2 = findViewById(R.id.viewPager);
         viewPagerAdapter = new ViewPagerAdapter(this);
         viewPager2.setAdapter(viewPagerAdapter);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //navigationView = findViewById(R.id.navigationDrawer);
+        //Menu menu = navigationView.getMenu();
+        //setUpNavigationView(navigationView, menu);
+
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
@@ -28,14 +44,15 @@ public class fragmentmain extends AppCompatActivity {
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-
+                // Kode untuk event saat tab tidak dipilih
             }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
-
+                // Kode untuk event saat tab dipilih kembali
             }
         });
+
         viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageSelected(int position) {
@@ -43,5 +60,32 @@ public class fragmentmain extends AppCompatActivity {
                 tabLayout.getTabAt(position).select();
             }
         });
+    }
+
+    private void setUpNavigationView(final NavigationView navigationView, final Menu menu) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        Fragment selectedFragment = selectDrawerItem(menuItem);
+                        FragmentManager fragmentManager = getSupportFragmentManager();
+                        fragmentManager.beginTransaction().replace(R.id.toolbar, selectedFragment).commit();
+                        navigationView.setCheckedItem(menuItem.getItemId());
+                        setTitle(menuItem.getTitle());
+                        return true;
+                    }
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.nav_drawer_menu, menu);
+        return true;
+    }
+
+
+    private Fragment selectDrawerItem(MenuItem menuItem) {
+        // Implementasi untuk memilih Fragment berdasarkan item yang dipilih di NavigationView
+        return new Fragment(); // Ganti dengan fragment yang sesuai
     }
 }
